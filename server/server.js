2be9202006c13ff1cd898759847d2e3337af79b7
Abhishek Sharma <abhello12@gmail.com>
2025-06-14 18:27:33 +0530

    url update

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: ['https://image-generator-mu-eight.vercel.app', 'http://localhost:3000'],
+    origin: ['https://image-generator-mu-eight.vercel.app', 'https://image-generator-three-rouge.vercel.app', 'http://localhost:3000'],
     methods: ['GET', 'POST'],
     credentials: true
 }));
